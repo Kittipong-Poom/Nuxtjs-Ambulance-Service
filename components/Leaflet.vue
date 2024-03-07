23b6646876f7825bc372c2937dc4f393eb9e2241
Weerapat Tusnaimatakul <6431503056@lamduan.mfu.ac.th>
2024-03-07 16:09:24 +0700

    อัพหน่อยเดะ

--- components/Leaflet.vue
+++ components/Leaflet.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <div id="map" style="height: 500px;"></div>
-      <button @click="getCurrentLocation">ตำแหน่งของรถฉุกเฉิน</button>
+      
     </div>
   </template>
   
@@ -17,6 +17,11 @@
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(this.map);
+      if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(this.updateCurrentLocation, this.handleLocationError);
+    } else {
+        console.error("Geolocation is not supported by this browser.");
+    }
     },
     data() {
     return {
@@ -24,40 +29,38 @@
     };
     },
     methods: {
-      getCurrentLocation() {
-        // รับตำแหน่งปัจจุบันของผู้ใช้
-        navigator.geolocation.getCurrentPosition(position => {
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-  
-          // เพิ่มเครื่องหมายตำแหน่งปัจจุบัน
-          // สร้างเครื่องหมายแบบไอคอนกำหนดเอง
-        var redIcon = L.icon({
-        iconUrl: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-        popupAnchor: [0, -32]
-        });
+    updateCurrentLocation(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
 
-        // เพิ่ม marker โดยใช้ไอคอนที่กำหนดเอง
-        if (this.currentLocationMarker) {
-        this.map.removeLayer(this.currentLocationMarker);
-      }
+        if (!this.currentLocationMarker) {
+            var redIcon = L.icon({
+                iconUrl: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
+                iconSize: [32, 32],
+                iconAnchor: [16, 32],
+                popupAnchor: [0, -32]
+            });
 
-      // เพิ่มเครื่องหมายตำแหน่งปัจจุบันใหม่
-      this.currentLocationMarker = L.marker([latitude, longitude], { icon: redIcon }).addTo(this.map)
-        .bindPopup('ตำแหน่งล่าสุด')
-        .openPopup();
-
-  
-          // ย้ายมุมมองแผนที่ไปยังตำแหน่งปัจจุบัน
-          this.map.setView([latitude, longitude], 13);
-        }, error => {
-          // หากเกิดข้อผิดพลาดในการรับตำแหน่ง
-          console.error('Error getting location:', error);
-        });
-      }
+            this.currentLocationMarker = L.marker([latitude, longitude], { icon: redIcon }).addTo(this.map)
+                .bindPopup('ตำแหน่งล่าสุด')
+                .openPopup();
+        } else {
+            this.currentLocationMarker.setLatLng([latitude, longitude]);
+        }
+             // ตรวจสอบว่าผู้ใช้กำลังมองแผนที่หรือไม่
+        if (this.map.getBounds().contains([latitude, longitude])) {
+            // อัปเดตตำแหน่งแต่ไม่ซูมเอ้าท์
+            this.map.setView([latitude, longitude], this.map.getZoom());
+        } else {
+            // ซูมเข้าเฉพาะเมื่อผู้ใช้ไม่ได้มองแผนที่
+            this.map.setView([latitude, longitude], 13);
+        }
+    },
+    handleLocationError(error) {
+        console.error('Error getting location:', error);
     }
+}
+
   }
   </script>
   
@@ -65,6 +68,7 @@
   /* เพิ่มสไตล์เพื่อความสวยงาม (ไม่จำเป็น) */
   #map {
     width: 100%;
+
   }
   </style>
   
