6c90dccada552cfc74cd3bc4a21b51adeee7c49a
KITTIPONG WONGSADITYANON <6431503005@lamduan.mfu.ac.th>
2024-06-10 23:43:12 +0700

    check2

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@ export default {
   
   data() {
     return {
-      endpointUrl: process.env.NODE_ENV == 'development' ? 'http://localhost:5000' : process.env.REMOTE_SERVER,
+      endpointUrl: process.env.NODE_ENV == 'development' ? 'http://localhost:5000' : 'https://ambulance-fbf9.onrender.com',
       username: '',
       password: '',
       error: ''
