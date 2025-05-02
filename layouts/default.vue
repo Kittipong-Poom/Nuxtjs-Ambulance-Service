7b389019bd39828c67ac4b24b0af8dd41ff8c7d3
Kittipong Wongasditsayanon <kittipong.wong00@gmail.com>
2025-05-02 19:59:58 +0700

    fix ui disabled

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,73 +1,113 @@
 <template>
   <v-app>
     <header>
-      <nav scroll-behavior="elevate" class="navbar navbar-expand-custom navbar-mainbg mx-auto">
-        <button class="navbar-toggler" :class="{ disabled: isNavbarDisabled }" type="button" @click="toggleNavbar">
+      <nav
+        scroll-behavior="elevate"
+        class="navbar navbar-expand-custom navbar-mainbg mx-auto"
+      >
+        <button
+          v-if="isLoggedIn"
+          class="navbar-toggler"
+          :class="{ disabled: isNavbarDisabled }"
+          type="button"
+          @click="toggleNavbar"
+        >
           <i class="fas fa-bars text-white"></i>
         </button>
-        <div class="collapse navbar-collapse" :class="{ show: isNavbarOpen }" id="navbarSupportedContent">
+        <div
+          v-if="isLoggedIn"
+          class="collapse navbar-collapse"
+          :class="{ show: isNavbarOpen }"
+          id="navbarSupportedContent"
+        >
           <ul class="navbar-nav d-flex w-100">
             <div class="hori-selector active">
               <div class="left"></div>
               <div class="right"></div>
             </div>
             <li class="nav-item">
-              <router-link class="nav-link" :to="isLoggedIn ? '/Dashboard' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin"><i
-                  class="fas fa-chart-bar"></i>Dashboard</router-link>
-            </li>
-            <li class="nav-item ">
-              <router-link class="nav-link" :to="isLoggedIn ? '/Patient' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin"><i
-                  class="far fa-address-book"></i>ผู้ป่วยทั่วไป</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" :to="isLoggedIn ? '/QueueJob' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin"><i
-                  class="fa-solid fa-users"></i>จัดการคิวงาน</router-link>
+              <router-link
+                class="nav-link"
+                :to="isLoggedIn ? '/Dashboard' : '/'"
+                :class="{ 'disabled-link': !isLoggedIn }"
+                @click.prevent="!isLoggedIn && redirectToLogin"
+                ><i class="fas fa-chart-bar"></i>Dashboard</router-link
+              >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" :to="isLoggedIn ? '/Calendars' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin"><i
-                  class="far fa-calendar-alt"></i>ปฏิทิน</router-link>
+              <router-link
+                class="nav-link"
+                :to="isLoggedIn ? '/Patient' : '/'"
+                :class="{ 'disabled-link': !isLoggedIn }"
+                @click.prevent="!isLoggedIn && redirectToLogin"
+                ><i class="far fa-address-book"></i>ผู้ป่วยทั่วไป</router-link
+              >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" :to="isLoggedIn ? '/Queueurgent' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin">
+              <router-link
+                class="nav-link"
+                :to="isLoggedIn ? '/QueueJob' : '/'"
+                :class="{ 'disabled-link': !isLoggedIn }"
+                @click.prevent="!isLoggedIn && redirectToLogin"
+                ><i class="fa-solid fa-users"></i>จัดการคิวงาน</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                :to="isLoggedIn ? '/Calendars' : '/'"
+                :class="{ 'disabled-link': !isLoggedIn }"
+                @click.prevent="!isLoggedIn && redirectToLogin"
+                ><i class="far fa-calendar-alt"></i>ปฏิทิน</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link
+                class="nav-link"
+                :to="isLoggedIn ? '/Queueurgent' : '/'"
+                :class="{ 'disabled-link': !isLoggedIn }"
+                @click.prevent="!isLoggedIn && redirectToLogin"
+              >
                 <i class="fas fa-ambulance"></i>ผู้ป่วยฉุกเฉิน
               </router-link>
             </li>
-            <!-- <li class="nav-item">
-              <router-link class="nav-link" :to="isLoggedIn ? '/Maps' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin">
-                <i class="fas fa-location-crosshairs"></i>แผนที่พิกัดรถฉุกเฉิน
-              </router-link>
-            </li> -->
             <li class="nav-item">
-              <router-link class="nav-link" :to="isLoggedIn ? '/MapsStaticAppointment' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin">
+              <router-link
+                class="nav-link"
+                :to="isLoggedIn ? '/MapsStaticAppointment' : '/'"
+                :class="{ 'disabled-link': !isLoggedIn }"
+                @click.prevent="!isLoggedIn && redirectToLogin"
+              >
                 <i class="fas fa-map"></i>แผนที่การนัดหมาย
               </router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" :to="isLoggedIn ? '/MapsStaticUrgent' : '/'"
-                :class="{ 'disabled-link': !isLoggedIn }" @click.prevent="!isLoggedIn && redirectToLogin">
+              <router-link
+                class="nav-link"
+                :to="isLoggedIn ? '/MapsStaticUrgent' : '/'"
+                :class="{ 'disabled-link': !isLoggedIn }"
+                @click.prevent="!isLoggedIn && redirectToLogin"
+              >
                 <i class="fas fa-location-dot"></i>แผนที่ฉุกเฉิน
               </router-link>
             </li>
             <v-tooltip>
               <template v-slot:activator="{ on, attrs }">
-                <button v-if="isLoggedIn" class="ml-auto mt-2 Btn-logout mr-3" @click="logout">
+                <button
+                  v-if="isLoggedIn"
+                  class="ml-auto mt-2 Btn-logout mr-3"
+                  @click="logout"
+                >
                   <div class="sign">
                     <svg viewBox="0 0 512 512">
                       <path
-                        d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z">
-                      </path>
+                        d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"
+                      ></path>
                     </svg>
                   </div>
                   <div class="text">ออกจากระบบ</div>
                 </button>
-              </template>       
+              </template>
             </v-tooltip>
           </ul>
         </div>
@@ -82,10 +122,9 @@
 </template>
 
 <script>
-import DefaultJs from '../scripts/Default.js';
+import DefaultJs from "../scripts/Default.js";
 
-
-import '../styles/Default.css'; // Import the CSS file
+import "../styles/Default.css";
 
 export default DefaultJs;
 </script>
